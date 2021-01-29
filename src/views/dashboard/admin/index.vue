a081537aa536a169913028c71b7c8bddc8c17ae1
superelite123 <74247167+martimbrito808@users.noreply.github.com>
2021-01-29 12:13:35 +0800

    clear dashboard

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -1,30 +1,5 @@
 <template>
-  <div class="dashboard-editor-container">
-
-    <panel-group @handleSetLineChartData="handleSetLineChartData" />
-
-    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
-      <line-chart :chart-data="lineChartData" />
-    </el-row>
-
-    <el-row :gutter="32">
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <raddar-chart />
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <pie-chart />
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <bar-chart />
-        </div>
-      </el-col>
-    </el-row>
-  </div>
+  <p>Dashboard</p>
 </template>
 
 <script>
